d17a9453eafbffeee264c8ef1d3f679ffe12f7c4
jpc gamer <julia.peres.cardoso@gmail.com>
2025-05-06 17:58:50 -0300

    Projeto Contador Funcional

--- aula04-joao/aula04/app/index.tsx
+++ aula04-joao/aula04/app/index.tsx
@@ -1,15 +1,8 @@
-import { Text, View } from "react-native";
+import Cont from './components/Contador';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+export default function App() {
+  return (    
+    <Cont />    
   );
 }
+
