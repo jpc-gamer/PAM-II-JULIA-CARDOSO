fea8e2e6519781a67e3aa00ce902309fe9d0c3dc
jpc gamer <julia.peres.cardoso@gmail.com>
2025-06-22 18:27:54 -0300

    exercicios fixação - 01

--- aula05/exercícios-ts/ex01.ts
+++ aula05/exercícios-ts/ex01.ts
@@ -1,3 +1,13 @@
 let produto: string = 'Mouse';
 let preco: number = 59.90;
 let disponivel: boolean = true;
+
+function exibirProduto(produto: string, preco: number, disponivel: boolean): void {
+    console.log (`Produto: ${produto} - Preço: R$${preco} - Disponível: ${disponivel ? 'Sim' : 'Não'}`);
+}
+
+exibirProduto(produto, preco, disponivel);
+
+console.log("a") 
+
+
